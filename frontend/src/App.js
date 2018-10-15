2cf89ff9d8cca2f1c45e1be9c011a7506ef403ff
denverquane <chewysnarf@gmail.com>
2018-10-15 01:49:29 -0600

    Added charts and made the UI (slightly) prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Card, Button, Collapse } from '@blueprintjs/core'
+import { LineChart, XAxis, YAxis, CartesianGrid, Line, Tooltip} from 'recharts'
+
+let Months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 class App extends Component {
     state = {
@@ -22,20 +26,26 @@ class App extends Component {
     );
   }
   displaySubs() {
-    let arr = []
-    for (var key in this.state.Subs) {
-      console.log(key)
-      arr.push(<div key={key}>
-        {key}:
-        <p>
-          Extracted: {this.state.Subs[key].Extracting ? 'True' : 'False'}
-          </p>
-          <p>
-          Processing: {this.state.Subs[key].Processing ? 'True' : 'False'}
-          </p>
-        </div>)
+    let arr = [];
+    for (let key in this.state.Subs) {
+        let months = [];
+        for (let mo in Months) {
+            months.push({key: Months[mo], comments: this.state.Subs[key].ExtractedMonthCommentCounts[Months[mo]]})
+        }
+      arr.push(
+
+          <div key={key} style={{display: 'flex', flexDirection: 'column'}}>
+              {key}
+              <CollapseExample text={months}/>
+          </div>);
+        arr.sort(function(a,b){
+            var x = a.key.toLowerCase();
+            var y = b.key.toLowerCase();
+            if (x < y) {return -1;}
+            if (x > y) {return 1;}
+            return 0;})
     }
-    return arr
+      return arr
   }
 
   getBlocks() {
@@ -45,12 +55,50 @@ class App extends Component {
           }).then(data => {
          
           if (JSON.stringify(this.state.Subs) !== JSON.stringify(data)) {
-            console.log('Updated');
+            // console.log('Updated');
             this.setState({ ...this.state, Subs: data });
           }
         
       });
   }
+}
+
+export class CollapseExample extends React.Component {
+
+    constructor(text) {
+        super();
+        this.state={
+            isOpen: false,
+            text: text,
+        }
+    }
+    state = {
+        isOpen: false,
+        text: null
+    };
+
+    render() {
+        return (
+            <div>
+                <Button onClick={this.handleClick}>
+                    {this.state.isOpen ? "Hide" : "Show"} Details
+                </Button>
+                <Collapse isOpen={this.state.isOpen}>
+                    <LineChart width={800} height={300} data={this.state.text.text} margin={{left: 30}}>
+                        <XAxis dataKey="key"/>
+                        <YAxis dataKey="comments"/>
+                        <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
+                        <Line type="monotone" dataKey="comments" stroke="black" />\
+                        <Tooltip/>
+                    </LineChart>
+                </Collapse>
+            </div>
+        );
+    }
+
+    handleClick = () => {
+        this.setState({ isOpen: !this.state.isOpen });
+    }
 }
 
 export default App;
